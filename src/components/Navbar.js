d9166fb4a1d9cb216587a5199857453fd2531658
ac1dgoddess <ac1dg0dd3ss@gmail.com>
2020-01-30 17:49:57 -0600

    initial changes for redoing this because i'm legit stupid

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/badassarmy-logo.jpg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -35,14 +35,14 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav
-        className="navbar is-transparent"
+        className="navbar has-background-black"
         role="navigation"
         aria-label="main-navigation"
       >
-        <div className="container">
-          <div className="navbar-brand">
+        <div className="container has-background-black">
+          <div className="navbar-brand has-background-black">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} />
             </Link>
             {/* Hamburger menu */}
             <div
